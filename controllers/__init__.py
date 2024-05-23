48c1854c2988c8fa50d12bea85236d5a158e67a5
Juan Almeida <juan.2003.heloisa@gmail.com>
2024-05-23 14:49:02 -0300

    Teste agendamento

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,3 +1,4 @@
 from .cadastro_clientes import *
 from .cadastro_produtos import *
+from .agenda import *
 from .login import *
--- init.sql
+++ init.sql
@@ -1,5 +1,6 @@
 CREATE schema tatoo_shop;
 USE tatoo_shop;
+
 CREATE TABLE tbl_cliente(
     usuario_id BIGINT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL, idade INT NOT NULL,
@@ -12,7 +13,6 @@ CREATE TABLE tbl_cliente(
     PRIMARY KEY (usuario_id)
     );
 
-USE tatoo_shop;
 CREATE TABLE tbl_produtos(
     produto_id BIGINT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL, marca VARCHAR(45) NOT NULL,
@@ -21,15 +21,23 @@ CREATE TABLE tbl_produtos(
     PRIMARY KEY (produto_id)
     );
 
-USE tatoo_shop;
-CREATE TABLE tbl_agendamento(
-    horarios VARCHAR(5) NOT NULL
+CREATE TABLE tbl_horas(
+    horarios VARCHAR(45) NOT NULL
 );
 
-USE tatoo_shop;
+CREATE TABLE tbl_agenda(
+    agenda_id BIGINT NOT NULL ,
+    dia DATE NOT NULL,
+    horario VARCHAR(45) NOT NULL,
+    telefone VARCHAR(12) NOT NULL,
+    PRIMARY KEY (agenda_id)
+    );
+
 INSERT INTO tbl_cliente (nome, idade, username, email, endereco, telefone, senha)
 VALUES ("admin", 00, "admin", "admin@admin.com", "admin", "admin", "admin");
 
-USE tatoo_shop;
-INSERT INTO tbl_agendamento (horarios)
-VALUES ("09h"), ("10h"), ("11h"), ("12h"), ("13h"), ("14h"), ("15h"), ("16h"), ("17h"), ("18h");
+INSERT INTO tbl_horas (horarios)
+VALUES ("09h"), ("10h"), ("11h"), ("13h"), ("14h"), ("15h"), ("16h"), ("17h");
+
+INSERT INTO tbl_agenda (agenda_id, dia, horario, telefone)
+VALUES (1, "2024-05-01", "11h", "111111");
